2b2b49d566d62eaf11d83528ef5967da3d10db2b
Jan Noha <nohajc@gmail.com>
2025-03-15 23:46:10 +0100

    using the sys-mount crate instead of rustix, it supports filesystem auto-detection

--- vmproxy/src/main.rs
+++ vmproxy/src/main.rs
@@ -1,5 +1,6 @@
-use rustix::mount::{MountFlags, mount};
+use anyhow::Context;
 use std::fs;
+use sys_mount::{FilesystemType, Mount, MountFlags, SupportedFilesystems};
 
 fn list_dir(dir: &str) {
     match fs::read_dir(dir) {
@@ -14,26 +15,36 @@ fn list_dir(dir: &str) {
     }
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     println!("Hello, world, from linux microVM!");
 
-    match fs::create_dir_all("/mnt/hostblk") {
-        Ok(_) => println!("Directory '/mnt/hostblk' created successfully."),
-        Err(e) => eprintln!("Failed to create directory '/mnt/hostblk': {}", e),
+    fs::create_dir_all("/mnt/hostblk").context("Failed to create directory '/mnt/hostblk'")?;
+    println!("Directory '/mnt/hostblk' created successfully.");
+
+    let supported_fs =
+        SupportedFilesystems::new().context("Failed to get supported filesystems")?;
+
+    for fs in supported_fs.dev_file_systems() {
+        println!("Supported filesystem: {:?}", fs);
     }
 
-    match mount(
-        "/dev/vda",
-        "/mnt/hostblk",
-        "btrfs",
-        MountFlags::RDONLY,
-        None,
-    ) {
-        Ok(_) => println!("'/dev/vda' mounted successfully on '/mnt/hostblk'."),
-        Err(e) => eprintln!("Failed to mount '/dev/vda' on '/mnt/hostblk': {}", e),
+    for fs in supported_fs.nodev_file_systems() {
+        println!("Supported nodev filesystem: {:?}", fs);
     }
+
+    let result = Mount::builder()
+        .fstype(FilesystemType::from(&supported_fs))
+        .flags(MountFlags::RDONLY)
+        .mount("/dev/vda", "/mnt/hostblk")
+        .context("Failed to mount '/dev/vda' on '/mnt/hostblk'")?;
+
+    println!(
+        "'/dev/vda' mounted successfully on '/mnt/hostblk', recognized as {}.",
+        result.get_fstype()
+    );
 
     list_dir("/mnt/hostblk/home/nohajc");
 
     // std::thread::sleep(std::time::Duration::from_secs(30));
+    Ok(())
 }
