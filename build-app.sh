c34a45044c0c0874a93b303cd81ec30d75983327
Jan Noha <nohajc@gmail.com>
2025-03-21 01:28:54 +0100

    removed cargo workspace because it doesn't play well with vscode when compiling each create for a different target

--- build-app.sh
+++ build-app.sh
@@ -13,9 +13,9 @@ fi
 cd "$SCRIPT_DIR"
 
 (cd "anylinuxfs" && cargo build $BUILD_ARGS)
-mkdir -p bin && cp "target/$BUILD_DIR/anylinuxfs" bin/
+mkdir -p bin && cp "anylinuxfs/target/$BUILD_DIR/anylinuxfs" bin/
 
 (cd "vmproxy" && cargo build $BUILD_ARGS)
-mkdir -p bin/vmroot && cp "target/aarch64-unknown-linux-musl/$BUILD_DIR/vmproxy" bin/vmroot/
+mkdir -p bin/vmroot && cp "vmproxy/target/aarch64-unknown-linux-musl/$BUILD_DIR/vmproxy" bin/vmroot/
 
 codesign --entitlements "anylinuxfs.entitlements" --force -s - bin/anylinuxfs
