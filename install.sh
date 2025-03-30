51fcd148d26ad411bee95d937dc34fe60a619d5c
Jan Noha <nohajc@gmail.com>
2025-03-30 21:44:53 +0200

    added install script

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PREFIX="/opt/anylinuxfs"
+
+sudo mkdir -p "$PREFIX"
+mkdir -p "$PREFIX/bin"
+mkdir -p "$PREFIX/libexec"
+
+sudo chown $(whoami):admin "$PREFIX"
+cp bin/* "$PREFIX/bin/"
+
+function fix_libkrun_path() {
+    install_name_tool -change \
+        /usr/local/lib/libkrun.dylib \
+        "$PREFIX/lib/libkrun.dylib" "$1" 2>/dev/null
+}
+
+fix_libkrun_path "$PREFIX/bin/anylinuxfs"
+codesign --entitlements "anylinuxfs.entitlements" --force -s - "$PREFIX/bin/anylinuxfs"
+
+fix_libkrun_path "$PREFIX/bin/fetch-rootfs"
+codesign --entitlements "anylinuxfs.entitlements" --force -s - "$PREFIX/bin/fetch-rootfs"
+
+cp -RL libexec/* "$PREFIX/libexec/"
