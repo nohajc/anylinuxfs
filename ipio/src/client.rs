6f3ec5cee53a05c231a01d2094d30918a1f94689
Jan Noha <nohajc@gmail.com>
2025-06-14 16:35:03 +0200

    implemented IPC client

--- ipio/src/client.rs
+++ ipio/src/client.rs
@@ -3,14 +3,20 @@
 
 use std::{
     cell::RefCell,
+    cmp::min,
     collections::{HashMap, hash_map::Entry},
+    error::Error,
+    mem::MaybeUninit,
+    slice,
     sync::{LazyLock, Mutex, atomic::AtomicI32},
+    thread,
 };
 
 use anyhow::Context;
 use iceoryx2::{
     port::client::Client, prelude::*, service::port_factory::request_response::PortFactory,
 };
+use libc::{c_int, off_t, ssize_t};
 
 use crate::{IORequest, IOResponse};
 
@@ -70,7 +76,7 @@ pub fn new_service(service_name: impl AsRef<str>) -> anyhow::Result<SvcHandle> {
 // the client in thread-local storage and pass it to `f`
 pub fn with_client<R>(
     handle: SvcHandle,
-    f: impl FnOnce(&mut ClientType) -> R,
+    f: impl FnOnce(&mut ClientType) -> anyhow::Result<R>,
 ) -> anyhow::Result<R> {
     CLIENTS.with(|clients| {
         let mut clients = clients.borrow_mut();
@@ -82,6 +88,12 @@ pub fn with_client<R>(
                 let service_name = service_names
                     .get(&handle)
                     .ok_or_else(|| anyhow::anyhow!("Service handle {:?} not found", handle))?;
+
+                println!(
+                    "Initializing new client for service: {} (thread: {:?})",
+                    service_name,
+                    thread::current().id()
+                );
 
                 let services = SERVICES.lock().unwrap();
                 let svc = services
@@ -99,6 +111,202 @@ pub fn with_client<R>(
             }
         };
 
-        Ok(f(client))
+        f(client)
     })
+}
+
+#[derive(Debug)]
+struct ErrnoError(libc::c_int);
+
+impl ErrnoError {
+    fn value(&self) -> libc::c_int {
+        self.0
+    }
+}
+
+impl std::fmt::Display for ErrnoError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "IO error: {}", self.0)
+    }
+}
+
+impl Error for ErrnoError {}
+
+pub unsafe fn readv(hnd: c_int, iov: *const libc::iovec, iovcnt: c_int, offset: off_t) -> ssize_t {
+    let iovecs = unsafe { slice::from_raw_parts(iov, iovcnt as usize) };
+    let total_buf_len = iovecs.iter().map(|iov| iov.iov_len as usize).sum();
+
+    let res = with_client(SvcHandle(hnd), |client| -> anyhow::Result<ssize_t> {
+        let mut req_data = client.loan_slice_uninit(0)?;
+        let req = req_data.user_header_mut();
+        *req = IORequest::Read {
+            size: total_buf_len,
+            offset,
+        };
+        let req = unsafe { req_data.assume_init() };
+        let pending_resp = req.send()?;
+
+        if let Some(resp) = pending_resp.receive()? {
+            let resp_data = resp.payload();
+            let resp_header = resp.user_header();
+            match *resp_header {
+                IOResponse::Read { size } => {
+                    if size < 0 {
+                        return Err(anyhow::anyhow!("Unexpecteed read error"));
+                    }
+
+                    let mut buf_pos = 0;
+                    let mut remaining_size = size as usize;
+                    for iov in iovecs {
+                        let buf = unsafe {
+                            slice::from_raw_parts_mut(iov.iov_base as *mut u8, iov.iov_len as usize)
+                        };
+                        let buf_size = min(buf_pos + iov.iov_len as usize, remaining_size);
+                        buf.copy_from_slice(&resp_data[buf_pos..buf_size]);
+                        buf_pos += iov.iov_len as usize;
+                        remaining_size -= iov.iov_len as usize;
+                    }
+
+                    return Ok(size);
+                }
+                IOResponse::Error { errno } => {
+                    return Err(ErrnoError(errno).into());
+                }
+                _ => {
+                    return Err(anyhow::anyhow!(
+                        "Unexpected response header: {:?}",
+                        resp_header
+                    ));
+                }
+            }
+        }
+
+        return Err(anyhow::anyhow!("No response received"));
+    });
+
+    match res {
+        Ok(size) => return size,
+        Err(e) => {
+            if let Some(errno) = e.downcast_ref::<ErrnoError>() {
+                unsafe {
+                    *libc::__error() = errno.value();
+                }
+            } else {
+                eprintln!("Error in readv: {:#}", e);
+                unsafe {
+                    *libc::__error() = libc::EINVAL;
+                }
+            }
+            return -1;
+        }
+    }
+}
+
+pub unsafe fn writev(hnd: c_int, iov: *const libc::iovec, iovcnt: c_int, offset: off_t) -> ssize_t {
+    let iovecs = unsafe { slice::from_raw_parts(iov, iovcnt as usize) };
+    let total_buf_len: usize = iovecs.iter().map(|iov| iov.iov_len as usize).sum();
+
+    let res = with_client(SvcHandle(hnd), |client| -> anyhow::Result<ssize_t> {
+        let mut req = client.loan_slice_uninit(total_buf_len)?;
+        let req_header = req.user_header_mut();
+        *req_header = IORequest::Write { offset };
+        let req_data = req.payload_mut();
+
+        let mut buf_pos = 0;
+        for iov in iovecs {
+            // iovec buffer contents should be initialized by the caller
+            // we just use MaybeUninit type so it is compatible with req_data
+            let buf = unsafe {
+                slice::from_raw_parts(iov.iov_base as *const MaybeUninit<u8>, iov.iov_len as usize)
+            };
+            req_data[buf_pos..buf_pos + iov.iov_len as usize].copy_from_slice(buf);
+            buf_pos += iov.iov_len as usize;
+        }
+
+        let req = unsafe { req.assume_init() };
+        let pending_resp = req.send()?;
+
+        if let Some(resp) = pending_resp.receive()? {
+            let resp_header = resp.user_header();
+            match *resp_header {
+                IOResponse::Write { size } => {
+                    if size < 0 {
+                        return Err(anyhow::anyhow!("Unexpected write error"));
+                    }
+                    return Ok(size);
+                }
+                IOResponse::Error { errno } => {
+                    return Err(ErrnoError(errno).into());
+                }
+                _ => {
+                    return Err(anyhow::anyhow!(
+                        "Unexpected response header: {:?}",
+                        resp_header
+                    ));
+                }
+            }
+        }
+
+        return Err(anyhow::anyhow!("No response received"));
+    });
+
+    match res {
+        Ok(size) => return size,
+        Err(e) => {
+            if let Some(errno) = e.downcast_ref::<ErrnoError>() {
+                unsafe {
+                    *libc::__error() = errno.value();
+                }
+            } else {
+                eprintln!("Error in writev: {:#}", e);
+                unsafe {
+                    *libc::__error() = libc::EINVAL;
+                }
+            }
+            return -1;
+        }
+    }
+}
+
+pub unsafe fn size(hnd: c_int) -> ssize_t {
+    let res = with_client(SvcHandle(hnd), |client| -> anyhow::Result<ssize_t> {
+        let mut req_data = client.loan_slice_uninit(0)?;
+        let req = req_data.user_header_mut();
+        *req = IORequest::Size;
+        let req = unsafe { req_data.assume_init() };
+        let pending_resp = req.send()?;
+
+        if let Some(resp) = pending_resp.receive()? {
+            let resp_header = resp.user_header();
+            match *resp_header {
+                IOResponse::Size { size } => return Ok(size),
+                IOResponse::Error { errno } => return Err(ErrnoError(errno).into()),
+                _ => {
+                    return Err(anyhow::anyhow!(
+                        "Unexpected response header: {:?}",
+                        resp_header
+                    ));
+                }
+            }
+        }
+
+        return Err(anyhow::anyhow!("No response received"));
+    });
+
+    match res {
+        Ok(size) => return size,
+        Err(e) => {
+            if let Some(errno) = e.downcast_ref::<ErrnoError>() {
+                unsafe {
+                    *libc::__error() = errno.value();
+                }
+            } else {
+                eprintln!("Error in size: {:#}", e);
+                unsafe {
+                    *libc::__error() = libc::EINVAL;
+                }
+            }
+            return -1;
+        }
+    }
 }
