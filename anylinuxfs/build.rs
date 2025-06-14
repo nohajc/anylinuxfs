a8252aa813e1171e73192e1f9bf34ed658b62cea
Jan Noha <nohajc@gmail.com>
2025-06-14 18:02:30 +0200

    anylinuxfs custom_io (wip)

--- anylinuxfs/build.rs
+++ anylinuxfs/build.rs
@@ -1,3 +1,4 @@
 fn main() {
-    println!("cargo:rustc-link-search=/opt/homebrew/opt/libkrun/lib");
+    // println!("cargo:rustc-link-search=/opt/homebrew/opt/libkrun/lib");
+    println!("cargo:rustc-link-search=/Users/nohajan/gitprojs/3rd-party/libkrun/target/release");
 }
