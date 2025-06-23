be7667931b2e84a4877230c1e0a01d331bc7d298
Jan Noha <nohajc@gmail.com>
2025-06-24 00:05:33 +0200

    read-only mode definitely works!

--- ipio/src/bin/test-server.rs
+++ ipio/src/bin/test-server.rs
@@ -1,24 +1,54 @@
-use std::fs;
+use std::{fs, process::Command, thread};
 
 use anyhow::Context;
+use ipio::ServerBuilder;
 
 fn main() -> anyhow::Result<()> {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() != 3 {
-        eprintln!("Usage: {} <service_name> <file_name>", args[0]);
-        std::process::exit(1);
-    }
-    let service_name = &args[1];
-    let file_name = &args[2];
-    // println!("Service name: {}", service_name);
-    // println!("File name: {}", file_name);
+    let disk_ident = args
+        .iter()
+        .find(|arg| arg.starts_with("custom:"))
+        .context("Disk identifier not provided")?;
 
     let file = fs::OpenOptions::new()
         .read(true)
         .write(true)
-        .open(file_name)
+        .open(format!(
+            "/dev/r{}",
+            disk_ident.trim_start_matches("custom:")
+        ))
         .context("Failed to open file")?;
 
-    ipio::start_io_server(service_name, file).context("Failed to start IO server")?;
+    let server_builder = ServerBuilder::new(4194304).context("Failed to create server builder")?;
+    let args = args
+        .iter()
+        .skip(1)
+        .map(|s| {
+            if s.starts_with("custom:") {
+                format!("custom:{}", server_builder.conn_string())
+            } else {
+                s.clone()
+            }
+        })
+        .collect::<Vec<_>>();
+
+    // println!("args: {:?}", args);
+
+    let mut cmd = Command::new("bin/anylinuxfs");
+    cmd.args(args);
+    let (mut child, mut server) = server_builder.spawn_client(cmd)?;
+    let hnd = thread::spawn(move || {
+        server.serve(file).unwrap();
+    });
+
+    let status = child.wait().context("Failed to wait for child process")?;
+    if !status.success() {
+        return Err(anyhow::anyhow!(
+            "Child process exited with status: {}",
+            status
+        ));
+    }
+    hnd.join().unwrap();
+
     Ok(())
 }
