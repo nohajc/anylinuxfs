061187a3202b811099afd1b167b7cd837a209923
Jan Noha <nohajc@gmail.com>
2025-07-12 23:21:17 +0200

    updated download-dependencies.sh

--- download-dependencies.sh
+++ download-dependencies.sh
@@ -4,8 +4,8 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-IMAGE_ARCHIVE_NAME="linux-aarch64-Image-v6.6-nfs-lvm.tar.gz"
-IMAGE_ARCHIVE_URL="https://github.com/nohajc/libkrunfw/releases/download/v6.6-nfs-lvm/${IMAGE_ARCHIVE_NAME}"
+IMAGE_ARCHIVE_NAME="linux-aarch64-Image-v6.12.34-anylinuxfs.tar.gz"
+IMAGE_ARCHIVE_URL="https://github.com/nohajc/libkrunfw/releases/download/v6.12.34/${IMAGE_ARCHIVE_NAME}"
 
 GVPROXY_VERSION="0.8.6"
 GVPROXY_URL="https://github.com/containers/gvisor-tap-vsock/releases/download/v${GVPROXY_VERSION}/gvproxy-darwin"
