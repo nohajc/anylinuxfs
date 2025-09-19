9d82a5b32ec4fdbe897c3efbe8981ab66a6d2f36
Jan Noha <nohajc@gmail.com>
2025-09-19 16:37:38 +0200

    update download-dedpendencies.sh

--- download-dependencies.sh
+++ download-dependencies.sh
@@ -5,8 +5,9 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 IMAGE_ARCHIVE_NAME="linux-aarch64-Image-v6.12.34-anylinuxfs.tar.gz"
-# TODO: update
-IMAGE_ARCHIVE_URL="https://github.com/nohajc/libkrunfw/releases/download/v6.12.34-rev3/${IMAGE_ARCHIVE_NAME}"
+RELEASE_URL="https://github.com/nohajc/libkrunfw/releases/download/v6.12.34-rev4"
+IMAGE_ARCHIVE_URL="${RELEASE_URL}/${IMAGE_ARCHIVE_NAME}"
+MODULES_ARCHIVE_URL="${RELEASE_URL}/modules.tar.gz"
 
 GVPROXY_VERSION="0.8.7"
 GVPROXY_URL="https://github.com/containers/gvisor-tap-vsock/releases/download/v${GVPROXY_VERSION}/gvproxy-darwin"
@@ -16,6 +17,10 @@ curl -L -o "$IMAGE_ARCHIVE_NAME" "$IMAGE_ARCHIVE_URL"
 mkdir -p "libexec"
 tar xzf "$IMAGE_ARCHIVE_NAME" -C "libexec"
 rm "$IMAGE_ARCHIVE_NAME"
+
+curl -LO "$MODULES_ARCHIVE_URL"
+mkdir -p "lib"
+mv modules.tar.gz lib/
 
 curl -L -o libexec/gvproxy "$GVPROXY_URL"
 chmod +x libexec/gvproxy
