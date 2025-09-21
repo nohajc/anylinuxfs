c4e32d2e7a069f84058685033b7dda921e2307b7
Jan Noha <nohajc@gmail.com>
2025-09-21 16:44:45 +0200

    `anylinuxfs init`: install kernel modules from squashfs archive

--- download-dependencies.sh
+++ download-dependencies.sh
@@ -7,7 +7,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 IMAGE_ARCHIVE_NAME="linux-aarch64-Image-v6.12.34-anylinuxfs.tar.gz"
 RELEASE_URL="https://github.com/nohajc/libkrunfw/releases/download/v6.12.34-rev4"
 IMAGE_ARCHIVE_URL="${RELEASE_URL}/${IMAGE_ARCHIVE_NAME}"
-MODULES_ARCHIVE_URL="${RELEASE_URL}/modules.tar.gz"
+MODULES_ARCHIVE_NAME="modules.squashfs"
+MODULES_ARCHIVE_URL="${RELEASE_URL}/${MODULES_ARCHIVE_NAME}"
 
 GVPROXY_VERSION="0.8.7"
 GVPROXY_URL="https://github.com/containers/gvisor-tap-vsock/releases/download/v${GVPROXY_VERSION}/gvproxy-darwin"
@@ -20,7 +21,7 @@ rm "$IMAGE_ARCHIVE_NAME"
 
 curl -LO "$MODULES_ARCHIVE_URL"
 mkdir -p "lib"
-mv modules.tar.gz lib/
+mv ${MODULES_ARCHIVE_NAME} lib/
 
 curl -L -o libexec/gvproxy "$GVPROXY_URL"
 chmod +x libexec/gvproxy
