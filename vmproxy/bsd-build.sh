bf7a9ee9caeb669edb5080cf9aa5ad344fa2cdcf
Jan Noha <nohajc@gmail.com>
2025-11-09 17:53:14 +0100

    not sure why I have problems building libc

---
+++ vmproxy/bsd-build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is expected to be run on a FreeBSD host
+# (aarch64 is a Tier 3 platform and as such doesn't support cross-compilation)
+
+set -e
+
+cd $(dirname "$0")
+
+# hack to temporarily ignore the default toolchain specification
+mv .cargo .cargo_
+trap "mv .cargo_ .cargo" EXIT
+
+cargo build --target-dir target-bsd --release
