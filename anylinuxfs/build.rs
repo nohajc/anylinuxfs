1d2051a7210eb40f50a1133ad4e086b3005deb3b
Jan Noha <nohajc@gmail.com>
2025-12-02 13:16:28 +0100

    not sure why we need to set the search path now, but whatever

--- anylinuxfs/build.rs
+++ anylinuxfs/build.rs
@@ -1,6 +1,8 @@
 fn main() {
     if !cfg!(feature = "freebsd") {
         println!("cargo:rustc-link-search=/opt/homebrew/opt/libkrun/lib");
+    } else {
+        println!("cargo:rustc-link-search=/usr/local/lib");
     }
     println!(
         "cargo:rustc-link-search=framework={}",
