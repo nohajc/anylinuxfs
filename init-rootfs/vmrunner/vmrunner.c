3a0fa770f384b33a0979625a0c237e28877ac4d2
Jan Noha <nohajc@gmail.com>
2025-12-02 02:43:45 +0100

    workaround for libkrun RAM issue

--- init-rootfs/vmrunner/vmrunner.c
+++ init-rootfs/vmrunner/vmrunner.c
@@ -1,67 +1,66 @@
 #include <errno.h>
-#include <string.h>
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
 
 #include <libkrun.h>
 
 #include "vmrunner.h"
 
 error success() {
-    error e = {0, NULL, NULL};
+  error e = {0, NULL, NULL};
+  return e;
+}
+
+error krun_error(int err, const char *prefix) {
+  if (err < 0) {
+    error e = {-err, prefix, strerror(-err)};
     return e;
+  }
+  return success();
 }
 
-error krun_error(int err, const char* prefix) {
-    if (err < 0) {
-        error e = {-err, prefix, strerror(-err)};
-        return e;
-    }
-    return success();
-}
+bool is_error(int res) { return res < 0; }
 
-bool is_error(int res) {
-    return res < 0;
-}
+error setup_and_start_vm(const char *kernel_path, const char *root_path,
+                         const char *script_path) {
+  int ctx = krun_create_ctx();
+  if (is_error(ctx)) {
+    return krun_error(ctx, "configuration context error");
+  }
 
-error setup_and_start_vm(const char* kernel_path, const char* root_path, const char* script_path) {
-    int ctx = krun_create_ctx();
-    if (is_error(ctx)) {
-        return krun_error(ctx, "configuration context error");
-    }
+  int res = krun_set_vm_config(ctx, 1, 1152);
+  if (is_error(res)) {
+    return krun_error(res, "vm configuration error");
+  }
 
-    int res = krun_set_vm_config(ctx, 1, 512);
-    if (is_error(res)) {
-        return krun_error(res, "vm configuration error");
-    }
+  res = krun_set_root(ctx, root_path);
+  if (is_error(res)) {
+    return krun_error(res, "set root error");
+  }
 
-    res = krun_set_root(ctx, root_path);
-    if (is_error(res)) {
-        return krun_error(res, "set root error");
-    }
+  res = krun_set_workdir(ctx, "/");
+  if (is_error(res)) {
+    return krun_error(res, "set workdir error");
+  }
 
-    res = krun_set_workdir(ctx, "/");
-    if (is_error(res)) {
-        return krun_error(res, "set workdir error");
-    }
+  const char *const envp[] = {NULL};
+  const char *const argv[] = {"/bin/busybox", "sh", script_path, NULL};
 
-    const char *const envp[] = { NULL };
-    const char *const argv[] = { "/bin/busybox", "sh", script_path, NULL };
+  res = krun_set_exec(ctx, argv[0], &argv[1], envp);
+  if (is_error(res)) {
+    return krun_error(res, "set exec error");
+  }
 
-    res = krun_set_exec(ctx, argv[0], &argv[1], envp);
-    if (is_error(res)) {
-        return krun_error(res, "set exec error");
-    }
+  res = krun_set_kernel(ctx, kernel_path, 0, NULL, NULL);
+  if (is_error(res)) {
+    return krun_error(res, "set kernel error");
+  }
 
-    res = krun_set_kernel(ctx, kernel_path, 0, NULL, NULL);
-    if (is_error(res)) {
-        return krun_error(res, "set kernel error");
-    }
+  res = krun_start_enter(ctx);
+  if (is_error(res)) {
+    return krun_error(res, "start vm error");
+  }
 
-    res = krun_start_enter(ctx);
-    if (is_error(res)) {
-        return krun_error(res, "start vm error");
-    }
-
-    return success();
+  return success();
 }
