aa58e127d493ef6b0cab8b718fa751e8a09699f7
Jan Noha <nohajc@gmail.com>
2026-01-09 16:59:04 +0100

    we can always link against the homebrew libkrun version (1.17 supports booting BSD)

--- anylinuxfs/build.rs
+++ anylinuxfs/build.rs
@@ -1,9 +1,5 @@
 fn main() {
-    if !cfg!(feature = "freebsd") {
-        println!("cargo:rustc-link-search=/opt/homebrew/opt/libkrun/lib");
-    } else {
-        println!("cargo:rustc-link-search=/usr/local/lib");
-    }
+    println!("cargo:rustc-link-search=/opt/homebrew/opt/libkrun/lib");
     println!(
         "cargo:rustc-link-search=framework={}",
         "/System/Library/PrivateFrameworks"
