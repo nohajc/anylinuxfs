02d9f79c6f7735adfed0e17b2ae3960c8a76f49a
Jan Noha <nohajc@gmail.com>
2026-01-13 20:35:53 +0100

    vmctrl WaitForReport message (wip)

--- common-utils/src/vmctrl.rs
+++ common-utils/src/vmctrl.rs
@@ -3,9 +3,21 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub enum Request {
     Quit,
+    WaitForReport,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub enum Response {
     Ack,
+    Report(Report),
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+pub struct Report {
+    pub kernel_log: Vec<u8>,
+}
+impl Report {
+    pub fn new(kernel_log: Vec<u8>) -> Self {
+        Self { kernel_log }
+    }
 }
