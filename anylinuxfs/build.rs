c23c498564d63a6d3db1ddd1e8f1c90daab452fb
Jan Noha <nohajc@gmail.com>
2026-04-13 02:11:52 +0200

    removed libkrun dylib search path

--- anylinuxfs/build.rs
+++ anylinuxfs/build.rs
@@ -1,5 +1,4 @@
 fn main() {
-    println!("cargo:rustc-link-search=/opt/homebrew/opt/libkrun/lib");
     println!(
         "cargo:rustc-link-search=framework={}",
         "/System/Library/PrivateFrameworks"
